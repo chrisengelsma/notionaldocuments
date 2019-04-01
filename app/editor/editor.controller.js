e0510db65b2a64e3b09bd34bb279fb86db0c47d1
Notional Documents <notionaldocs@gmail.com>
2019-04-01 08:20:38 -0700

    ordered list

--- app/editor/editor.controller.js
+++ app/editor/editor.controller.js
@@ -97,7 +97,7 @@
 
 
     let create = document.getElementById('downloadlink');
-    let theBook = document.getElementById('tree-root').textContent;
+    let theBook = document.getElementById('bookdata').textContent;
     console.log(theBook);
     $scope.textFile = $scope.data[0];
 
