489afd30981d744c4f21972d7b4b34623fc820c5
Chris Engelsma <chris.engelsma@gmail.com>
2019-12-08 22:15:23 -0600

    fixed expanding-textareas (#8)

--- gulp/build.js
+++ gulp/build.js
@@ -73,11 +73,8 @@ gulp.task('html', ['inject', 'partials'], function() {
     .pipe($.sourcemaps.write('maps'))
     .pipe(jsFilter.restore)
     .pipe(cssFilter)
-    // .pipe($.sourcemaps.init())
-    // .pipe($.replace('../../bower_components/bootstrap-sass/assets/fonts/bootstrap/', '../fonts/'))
     .pipe($.cssnano())
     .pipe($.rev())
-    // .pipe($.sourcemaps.write('maps'))
     .pipe(cssFilter.restore)
     .pipe($.revReplace())
     .pipe(htmlFilter)
@@ -110,7 +107,7 @@ gulp.task('other', function() {
 
   return gulp.src([
     path.join(conf.paths.src, '/**/*'),
-    path.join('!' + conf.paths.src, '/**/*.{html,css,js,scss}')
+    path.join('!' + conf.paths.src, '/**/*.{html,css,js,scss}'),
   ])
     .pipe(fileFilter)
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
