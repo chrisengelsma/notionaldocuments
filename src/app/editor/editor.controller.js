7380c8cc67364a7f7a715d3814b08003c0dec74e
notionaldocuments <notionaldocuments@gmail.com>
2020-02-01 08:49:27 -0800

    dollar

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -255,7 +255,7 @@
         console.log('Selecting blank with function')
         var id = $scope.data[0].paragraphs[0].propositions[0].id;
         $scope.selectedProposition = $scope.data[0].paragraphs[0].propositions[0];
-        timeout( function(){
+        $timeout( function(){
           document.getElementById('proposition' + id).click();
         },0)         
       }
