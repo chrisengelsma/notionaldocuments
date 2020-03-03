83b9dbf6335c42e0461d047084d21a234961311b
notionaldocuments <notionaldocuments@gmail.com>
2020-03-03 07:55:55 -0800

    dont stop propagation

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -2750,7 +2750,7 @@
 
       $scope.getLastVisiblePropositionInParagraph = function (node, paragraph, event) {
 
-        if (event.target.localName !== 'ol' ){
+        if (event.target.localName !== 'ol' || event.target.localName !== 'li' ){
           console.log('returning')
           return;
         }
