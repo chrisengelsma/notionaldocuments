197f07274aaafc75162b56037aa9b95c54f99257
notionaldocuments <notionaldocuments@gmail.com>
2020-03-07 13:13:58 -0800

    install invert function

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -72,6 +72,8 @@
         })
       }
 
+
+
       function traverseObject(obj) {
         for (var key in obj) {
           if (obj.hasOwnProperty(key)) {
@@ -118,7 +120,35 @@
       // Have it do things when it's on paragraphs and propositions
   }
 
+    $scope.invert = function() {
+      var css = 'html {-webkit-filter: invert(100%);' +
+        '-moz-filter: invert(100%);' + 
+        '-o-filter: invert(100%);' + 
+        '-ms-filter: invert(100%); }',
+      head = document.getElementsByTagName('head')[0],
+      style = document.createElement('style');
 
+      if (!window.counter){ 
+        window.counter = 1;
+        } else  { 
+          window.counter ++;
+        }
+      if (window.counter % 2 == 0) { 
+        var css ='html {-webkit-filter: invert(0%); -moz-filter: invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }'
+      }
+ 
+
+      style.type = 'text/css';
+      if (style.styleSheet){
+        style.styleSheet.cssText = css;
+      } else {
+        style.appendChild(document.createTextNode(css));
+      }
+
+      head.appendChild(style);
+ 
+    }
+    
     $scope.openNewBookModal = function() {
       $scope.addBookModalInstance = $uibModal.open({
         animation: true,
