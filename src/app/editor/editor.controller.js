db78782c0c2b79360d2e53404717ba174797e287
notionaldocuments <notionaldocuments@gmail.com>
2020-03-15 14:47:48 -0700

    console logs

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -1175,12 +1175,16 @@
             console.log('Paragraph destination: ', apply.paragraphDestination)
             for (var i = 0; i < apply.paragraphDestination.propositions.length; i++){
               console.log('Hmm ', apply.paragraphDestination.propositions[i])
-              if(apply.paragraphDestination.propositions[i][$scope.userId] !== 'hidden'){
-                apply.paragraphDestination.owner = apply.paragraphDestination.propositions[i].author;
-                console.log('Owner: ', apply.paragraphDestination.owner)
-                console.log('Author: ', apply.paragraphDestination.propositions[i].author)
-                break;
-              }
+              console.log('Paragraph destination, about to go into first loop: ', apply.paragraphDestination)
+              if (apply.paragraphDestination.propositions[i][$scope.userId]){
+                console.log('Paragraph destination, about to go into second loop: ', apply.paragraphDestination)
+                if(apply.paragraphDestination.propositions[i][$scope.userId] !== 'hidden'){
+                  apply.paragraphDestination.owner = apply.paragraphDestination.propositions[i].author;
+                  console.log('Owner: ', apply.paragraphDestination.owner)
+                  console.log('Author: ', apply.paragraphDestination.propositions[i].author)
+                  break;
+                }
+              } else {console.log('Didnt make it')}
             }
 
             for (var i = 0; i < $scope.data[0].dialogue.length - 1; i++) {
