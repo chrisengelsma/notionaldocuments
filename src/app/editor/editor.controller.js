6367a3061ddf508a53c066abe83abd066548a93b
notionaldocuments <notionaldocuments@gmail.com>
2020-04-15 16:16:46 -0700

    esception

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -459,15 +459,13 @@
       }
 
       // If an empty book, focus on the blank proposition
-      // if (!$scope.data[0].paragraphs[0].propositions[0].author){
-      //   var id = $scope.data[0].paragraphs[0].propositions[0].id;
-      //   $scope.selectedProposition = $scope.data[0].paragraphs[0].propositions[0];
-      //   $timeout( function(){
-      //     document.getElementById('proposition' + id).click();
-      //   },0)
-      // }
-
-      // Rewrite the above
+      if ($scope.data[0].paragraphs[0].propositions[0].type === 'blank' && $scope.data[0].paragraphs[0].propositions[0][$scope.userId] !== 'hidden'){
+        var id = $scope.data[0].paragraphs[0].propositions[0].id;
+        $scope.selectedProposition = $scope.data[0].paragraphs[0].propositions[0];
+        $timeout( function(){
+          document.getElementById('proposition' + id).click();
+        },0)
+      }
 
       // If the data doesn't have a dialogue, make the dialogue empty
       if (!$scope.data[0].hasOwnProperty('dialogue')) {
