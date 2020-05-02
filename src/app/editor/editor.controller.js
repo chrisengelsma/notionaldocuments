e5aedde9611c0a127c8ebf59672493d62d68598a
notionaldocuments <notionaldocuments@gmail.com>
2020-05-01 22:43:31 -0700

    refactor

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -546,7 +546,7 @@
         $scope.selectedProposition = $scope.data[0].paragraphs[blankClickAssigned.paragraphPosition].propositions[blankClickAssigned.position];
         $timeout( function(){
           console.log("click assigned id: ", blankClickAssigned.id)
-          document.getElementById(blankClickAssigned.id).click();
+          document.getElementById('proposition'+ blankClickAssigned.id).click();
           console.log("Runs at startup?")
           blankClickAssigned = {};
         },300)
