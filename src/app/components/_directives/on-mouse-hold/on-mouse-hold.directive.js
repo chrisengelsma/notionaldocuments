495e86b1c29d2de899c32601ac5c492f248cc91a
notionaldocuments <notionaldocuments@gmail.com>
2020-06-26 11:00:13 -0700

    different defintino

--- src/app/components/_directives/on-mouse-hold/on-mouse-hold.directive.js
+++ src/app/components/_directives/on-mouse-hold/on-mouse-hold.directive.js
@@ -4,32 +4,35 @@
   /** @ngInject */
   function onMouseHold($parse, $interval) {
     var stop;
-    return {
-      restrict: 'A',
-      scope: { method: '&onMouseHold' },
-      link: function (scope, element, attrs) {
-        
-        var actionInterval = (attrs.mouseHoldRepeat) ? attrs.mouseHoldRepeat : 300;
-        
-        function startAction () {
-          $scope.dragProposition();
-          stop = $interval(function () {
-            $scope.dragProposition();
-          }, actionInterval);
-        }
 
-        var stopAction = function () {
-          if (stop) {
-            $interval.cancel(stop);
-            stop = undefined;
-          }
+        var dirDefObj = {
+            restrict: 'A',
+            scope: { method: '&onMouseHold' },
+            link: function (scope, element, attrs) {
+                var expressionHandler = scope.method();
+                var actionInterval = (attrs.mouseHoldRepeat) ? attrs.mouseHoldRepeat : 500;
+
+                var startAction = function () {
+                    expressionHandler();
+                    stop = $interval(function () {
+                        expressionHandler();
+                    }, actionInterval);
+                };
+
+                var stopAction = function () {
+                    if (stop) {
+                        $interval.cancel(stop);
+                        stop = undefined;
+                    }
+                };
+
+                element.bind('mousedown', startAction);
+                element.bind('mouseup', stopAction);
+                element.bind('mouseout', stopAction);
+            }
         };
 
-        element.bind('mousedown', startAction);
-        element.bind('mouseup', stopAction);
-        element.bind('mouseout', stopAction);
-      }
-    };
+        return dirDefObj;
   }
 
   angular.module('ndApp')
