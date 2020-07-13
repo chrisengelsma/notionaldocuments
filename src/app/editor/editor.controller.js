32562c4df928a86c0024d0d5506ebebdc8d51e63
notionaldocuments <notionaldocuments@gmail.com>
2020-07-12 21:33:09 -0700

    dragged proposition

--- src/app/editor/editor.controller.js
+++ src/app/editor/editor.controller.js
@@ -1466,12 +1466,13 @@
       }
 
       $scope.dropProposition = function(paragraph, proposition, flag){
+        console.log("Dragged proposition being dropped: ", $scope.draggedProposition)
         setTimeout(function() {
           $scope.$apply(function() {
-            if (!$scope.draggedProposition){
-              console.log("No drag")
-              return;
-            }
+            // if (!$scope.draggedProposition){
+            //   console.log("No drag")
+            //   return;
+            // }
             apply = {};
             console.log("Dropping")
             // apply.hideFast = document.getElementById('wholeprop' + $scope.draggedProposition.id);
