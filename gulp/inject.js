1e62018b1266fe2a99d8de3fd617adc6efdbfc9c
Chris Engelsma <chris.engelsma@gmail.com>
2020-07-22 21:43:39 -0500

    Fixed gulp watch

--- gulp/inject.js
+++ gulp/inject.js
@@ -1,27 +1,27 @@
 'use strict';
 
-var path = require('path');
-var gulp = require('gulp');
-var conf = require('./conf');
+const path = require('path');
+const gulp = require('gulp');
+const conf = require('./conf');
 
-var $ = require('gulp-load-plugins')();
+const $ = require('gulp-load-plugins')();
 
-var wiredep = require('wiredep').stream;
-var _ = require('lodash');
+const wiredep = require('wiredep').stream;
+const _ = require('lodash');
 
-var browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();
 
 gulp.task('inject-reload', ['inject'], function () {
   browserSync.reload();
 });
 
 gulp.task('inject', ['scripts', 'styles'], function () {
-  var injectStyles = gulp.src([
+  const injectStyles = gulp.src([
     path.join(conf.paths.tmp, '/serve/app/**/*.css'),
     path.join('!' + conf.paths.tmp, '/serve/app/vendor.css')
   ], { read: false });
 
-  var injectScripts = gulp.src([
+  const injectScripts = gulp.src([
     path.join(conf.paths.src, '/app/**/*.module.js'),
     path.join(conf.paths.src, '/app/**/*.js'),
     path.join(conf.paths.src, '/lib/*.js'),
@@ -31,7 +31,7 @@ gulp.task('inject', ['scripts', 'styles'], function () {
     .pipe($.angularFilesort())
     .on('error', conf.errorHandler('AngularFilesort'));
 
-  var injectOptions = {
+  const injectOptions = {
     ignorePath: [conf.paths.src, path.join(conf.paths.tmp, '/serve')],
     addRootSlash: false
   };
